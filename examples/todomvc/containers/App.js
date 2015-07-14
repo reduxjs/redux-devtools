72301fb74154049f39db204d1de4ddf9bb06cb9d
Dan Abramov <dan.abramov@gmail.com>
2015-07-15 00:25:02 +0300

    Add monitor to TodoMVC

--- examples/todomvc/containers/App.js
+++ examples/todomvc/containers/App.js
@@ -1,18 +1,32 @@
 import React, { Component } from 'react';
 import TodoApp from './TodoApp';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, compose } from 'redux';
+import { devTools, persistState } from 'redux-devtools';
+import { DevTools, DebugPanel, LogMonitor } from 'redux-devtools/lib/react';
 import { Provider } from 'react-redux';
 import * as reducers from '../reducers';
 
+const finalCreateStore = compose(
+  devTools(),
+  persistState(window.location.href.match(/[?&]debug_session=([^&]+)\b/)),
+  createStore
+);
+
 const reducer = combineReducers(reducers);
-const store = createStore(reducer);
+const store = finalCreateStore(reducer);
 
 export default class App extends Component {
   render() {
     return (
-      <Provider store={store}>
-        {() => <TodoApp /> }
-      </Provider>
+      <div>
+        <Provider store={store}>
+          {() => <TodoApp /> }
+        </Provider>
+        <DebugPanel top right bottom>
+          <DevTools store={store}
+                    monitor={LogMonitor} />
+        </DebugPanel>
+      </div>
     );
   }
 }
