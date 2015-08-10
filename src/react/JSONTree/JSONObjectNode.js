696b3bbc7f3a96c21783a51152384f7276e580fe
dzannotti <d.zannotti@me.com>
2015-08-10 23:33:31 +0100

    fixes https://github.com/dzannotti/redux-devtools/commit/021e95071f23d3c30fbf8790b3e42632a3338eaa#commitcomment-12626902

--- src/react/JSONTree/JSONObjectNode.js
+++ src/react/JSONTree/JSONObjectNode.js
@@ -76,7 +76,7 @@ export default class JSONObjectNode extends React.Component {
   getItemString() {
     if (!this.itemString) {
       const len = Object.keys(this.props.data).length;
-      this.itemString = len + ' key' + (len > 1 ? 's' : '');
+      this.itemString = len + ' key' + (len !== 0 ? 's' : '');
     }
     return this.itemString;
   }
