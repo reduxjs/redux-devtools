e76bb6956bce791c1eee2466694a68112adbd9c7
FredyC <fredy.c@seznam.cz>
2015-08-19 21:58:25 +0200

    custom parsers for persistState

--- src/persistState.js
+++ src/persistState.js
@@ -1,6 +1,50 @@
-export default function persistState(sessionId) {
+export default function persistState(sessionId, parsers) {
   if (!sessionId) {
     return next => (...args) => next(...args);
+  }
+
+  function callParser(parserMethod, parserName, stateSlice) {
+    const parser = parsers[parserName];
+    if (parser && typeof parser[parserMethod] === 'function') {
+      return parser[parserMethod](stateSlice);
+    }
+    return stateSlice;
+  }
+
+  // function serializeState(state) {
+  //   if (!state) {
+  //     return state;
+  //   }
+  //   return Object.keys(state).reduce((serialized, sliceName) => {
+  //     serialized[sliceName] = callParser('serialize', sliceName, state[sliceName]);
+  //     return serialized;
+  //   }, {});
+  // }
+
+  function deserializeState(state) {
+    if (!state) {
+      return state;
+    }
+    return Object.keys(state).reduce((deserialized, sliceName) => {
+      deserialized[sliceName] = callParser('deserialize', sliceName, state[sliceName]);
+      return deserialized;
+    }, {});
+  }
+
+  function parseState(fullState) {
+    if (!fullState || !parsers) {
+      return fullState;
+    }
+    return {
+      ...fullState,
+      committedState: deserializeState(fullState.committedState),
+      computedStates: fullState.computedStates.map((computedState) => {
+        return {
+          ...computedState,
+          state: deserializeState(computedState.state)
+        };
+      })
+    };
   }
 
   return next => (reducer, initialState) => {
@@ -8,7 +52,7 @@ export default function persistState(sessionId) {
 
     let finalInitialState;
     try {
-      finalInitialState = JSON.parse(localStorage.getItem(key)) || initialState;
+      finalInitialState = parseState(JSON.parse(localStorage.getItem(key))) || initialState;
       next(reducer, initialState);
     } catch (e) {
       console.warn('Could not read debug session from localStorage:', e);
