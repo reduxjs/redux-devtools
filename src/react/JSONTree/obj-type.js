04f15f6d4de80f09544048c9bc606853309293c2
FredyC <fredy.c@seznam.cz>
2015-08-22 06:13:04 +0200

    arrays shouldn't be treated as an iterable

--- src/react/JSONTree/obj-type.js
+++ src/react/JSONTree/obj-type.js
@@ -1,5 +1,7 @@
 export default function(obj) {
-  if (obj !== null && typeof obj === 'object' && typeof obj[Symbol.iterator] === 'function') {
+  if (obj !== null && typeof obj === 'object' && !Array.isArray(obj) &&
+    typeof obj[Symbol.iterator] === 'function'
+  ) {
     return 'Iterable';
   }
   return Object.prototype.toString.call(obj).slice(8, -1);
