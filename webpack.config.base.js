cc7037addb91f1e83d3dd55457cf93b0bfef261d
Tomer Ohana <tomero@wix.com>
2015-10-03 19:54:03 +0300

    Added UMD build

---
+++ webpack.config.base.js
@@ -0,0 +1,45 @@
+'use strict';
+
+var webpack = require('webpack');
+
+var reactExternal = {
+  root: 'React',
+  commonjs2: 'react',
+  commonjs: 'react',
+  amd: 'react'
+};
+
+var reactReduxExternal = {
+  root: 'ReactRedux',
+  commonjs2: 'react-redux',
+  commonjs: 'react-redux',
+  amd: 'react-redux'
+};
+
+var reduxExternal = {
+  root: 'Redux',
+  commonjs2: 'redux',
+  commonjs: 'redux',
+  amd: 'redux'
+};
+
+module.exports = {
+  externals: {
+    'react': reactExternal,
+    'react-native': reactExternal,
+    'react-redux': reactReduxExternal,
+    'redux': reduxExternal
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, loaders: ['babel-loader'], exclude: /node_modules/ }
+    ]
+  },
+  output: {
+    library: 'ReduxDevTools',
+    libraryTarget: 'umd'
+  },
+  resolve: {
+    extensions: ['', '.js']
+  }
+};
