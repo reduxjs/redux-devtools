a046f01b81af2b7325b3d295691983f4745a25cb
oviava <ovidiu.ava@gmail.com>
2015-10-16 13:29:26 +0300

    fix error - multiple spaces before Component

--- src/react/LogMonitor.js
+++ src/react/LogMonitor.js
@@ -1,4 +1,4 @@
-import React, { PropTypes,  Component } from 'react';
+import React, { PropTypes, Component } from 'react';
 import { findDOMNode } from 'react-dom';
 import LogMonitorEntry from './LogMonitorEntry';
 import LogMonitorButton from './LogMonitorButton';
