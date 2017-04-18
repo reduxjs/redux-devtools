3697b5e80f806f6c2f742a8202c0fb09698f448b
Timofey Smirnov <tim@driveback.ru>
2017-04-19 00:09:03 +0300

    fixed PropTypes warnings

--- src/createDevTools.js
+++ src/createDevTools.js
@@ -1,4 +1,5 @@
-import React, { Children, Component, PropTypes } from 'react';
+import React, { Children, Component } from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import instrument from 'redux-devtools-instrument';
 
