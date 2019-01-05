7c3b78d312d3d2b3d4cd8ec9147c68e5c0296b6e
Mihail Diordiev <zalmoxisus@gmail.com>
2019-01-05 19:38:06 +0200

    Create umd build for react-json-tree (#447)

---
+++ packages/react-json-tree/webpack.config.umd.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const webpack = require('webpack');
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = (env = {}) => (
+  {
+    mode: 'production',
+    entry: {
+      app: ['./src/index.js']
+    },
+    output: {
+      library: 'ReactJsonTree',
+      libraryTarget: 'umd',
+      path: path.resolve(__dirname, 'umd'),
+      filename: env.minimize ? 'react-json-tree.min.js' : 'react-json-tree.js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          loader: 'babel-loader',
+          exclude: /node_modules/
+        }
+      ]
+    },
+    externals: {
+      react: {
+        root: 'React',
+        commonjs2: 'react',
+        commonjs: 'react',
+        amd: 'react'
+      },
+      'react-dom': {
+        root: 'ReactDOM',
+        commonjs2: 'react-dom',
+        commonjs: 'react-dom',
+        amd: 'react-dom'
+      }
+    },
+    optimization: {
+      minimize: !!env.minimize,
+      minimizer: [
+        new TerserPlugin({
+          terserOptions: {
+            safari10: true
+          }
+        })
+      ]
+    },
+    performance: {
+      hints: false
+    }
+  }
+);
