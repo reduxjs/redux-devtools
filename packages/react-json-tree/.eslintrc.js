a9b4db9b9fd96ef638099bb976782fb7e2ac18e9
Nathan Bierema <nbierema@gmail.com>
2020-05-04 16:52:48 -0400

    Initial setup

---
+++ packages/react-json-tree/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json'],
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:prettier/recommended',
+    'prettier/@typescript-eslint',
+    'prettier/react'
+  ],
+};
