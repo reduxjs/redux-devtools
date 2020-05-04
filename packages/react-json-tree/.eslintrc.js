ae8eb20bd2034148b86b672b722ef6d649b1dc23
Nathan Bierema <nbierema@gmail.com>
2020-05-04 18:31:38 -0400

    Add react

--- packages/react-json-tree/.eslintrc.js
+++ packages/react-json-tree/.eslintrc.js
@@ -3,18 +3,22 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     tsconfigRootDir: __dirname,
-    project: ['./tsconfig.json'],
+    project: ['./tsconfig.json']
   },
-  plugins: [
-    '@typescript-eslint',
-  ],
+  plugins: ['@typescript-eslint', 'react'],
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:react/recommended',
     'plugin:prettier/recommended',
     'prettier/@typescript-eslint',
     'prettier/react'
   ],
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  }
 };
