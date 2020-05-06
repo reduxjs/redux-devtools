0f138922ffaf0038f457a636e315ce7311974f1b
Nathan Bierema <nbierema@gmail.com>
2020-05-06 12:50:51 -0400

    Centralize

--- packages/react-json-tree/.eslintrc.js
+++ packages/react-json-tree/.eslintrc.js
@@ -3,29 +3,18 @@ module.exports = {
   overrides: [
     {
       files: ['*.ts', '*.tsx'],
-      parser: '@typescript-eslint/parser',
+      extends: '../../eslintrc.ts.react.base.json',
       parserOptions: {
         tsconfigRootDir: __dirname,
-        project: ['./tsconfig.json'],
-        ecmaFeatures: {
-          jsx: true
-        }
-      },
-      plugins: ['@typescript-eslint', 'react'],
-      extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/eslint-recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:@typescript-eslint/recommended-requiring-type-checking',
-        'plugin:react/recommended',
-        'plugin:prettier/recommended',
-        'prettier/@typescript-eslint',
-        'prettier/react'
-      ],
-      settings: {
-        react: {
-          version: 'detect'
-        }
+        project: ['./tsconfig.json']
+      }
+    },
+    {
+      files: ['test/*.ts', 'test/*.tsx'],
+      extends: '../../eslintrc.ts.react.jest.base.json',
+      parserOptions: {
+        tsconfigRootDir: __dirname,
+        project: ['./tsconfig.test.json']
       }
     }
   ]
