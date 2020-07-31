7a915e62529c4a32ef13decc96e67101d5bfe457
Nathan Bierema <nbierema@gmail.com>
2020-07-31 14:46:14 -0400

    Consolidate dependencies in react-json-tree examples

--- packages/react-json-tree/examples/webpack.config.js
+++ packages/react-json-tree/examples/webpack.config.js
@@ -4,6 +4,7 @@ var webpack = require('webpack');
 var isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = {
+  mode: isProduction ? 'production' : 'development',
   devtool: 'source-map',
   entry: [
     !isProduction && 'webpack-dev-server/client?http://localhost:3000',
@@ -16,39 +17,32 @@ module.exports = {
     publicPath: '/static/'
   },
   plugins: [
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify(process.env.NODE_ENV)
-      }
-    }),
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoEmitOnErrorsPlugin(),
-    isProduction &&
-      new webpack.optimize.UglifyJsPlugin({
-        compress: { warnings: false },
-        output: { comments: false },
-        sourceMap: true
-      })
-  ].filter(Boolean),
+  ],
   resolve: {
     alias: {
       'react-json-tree/lib': path.join(__dirname, '..', 'src'),
-      'react-json-tree': path.join(__dirname, '..', 'src'),
-      react: path.join(__dirname, 'node_modules', 'react')
+      'react-json-tree': path.join(__dirname, '..', 'src')
     },
     extensions: ['.js']
   },
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.js$/,
-        loaders: ['babel-loader'].filter(Boolean),
-        include: path.join(__dirname, 'src')
+        loader: 'babel-loader',
+        include: path.join(__dirname, 'src'),
+        options: {
+          rootMode: 'upward'
+        }
       },
       {
         test: /\.js$/,
-        loaders: ['babel-loader'].filter(Boolean),
-        include: path.join(__dirname, '..', 'src')
+        loader: 'babel-loader',
+        include: path.join(__dirname, '..', 'src'),
+        options: {
+          rootMode: 'upward'
+        }
       }
     ]
   }
