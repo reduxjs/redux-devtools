9c68d51d7247ddb2cafb1257cceb0d5498746b4e
Nathan Bierema <nbierema@gmail.com>
2020-08-01 18:49:38 -0400

    Upgrade knex

--- packages/redux-devtools-cli/src/db/migrations/index.js
+++ packages/redux-devtools-cli/src/db/migrations/index.js
@@ -1,4 +1,4 @@
-exports.up = function(knex, Promise) {
+exports.up = function(knex) {
   return Promise.all([
     knex.schema.createTable('remotedev_reports', function(table) {
       table.uuid('id').primary();
@@ -77,7 +77,7 @@ exports.up = function(knex, Promise) {
   ]);
 };
 
-exports.down = function(knex, Promise) {
+exports.down = function(knex) {
   return Promise.all([
     knex.schema.dropTable('remotedev_reports'),
     knex.schema.dropTable('remotedev_apps')
