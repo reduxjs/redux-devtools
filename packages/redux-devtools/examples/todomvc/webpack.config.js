657eb4163ecad790fbb2224e34bd45d26489f093
Nathan Bierema <nbierema@gmail.com>
2020-08-01 15:07:52 -0400

    chore(redux-devtools): resolve warnings in examples

--- packages/redux-devtools/examples/todomvc/webpack.config.js
+++ packages/redux-devtools/examples/todomvc/webpack.config.js
@@ -2,7 +2,8 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-  devtool: 'eval',
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+  devtool: 'eval-source-map',
   entry: [
     'webpack-dev-server/client?http://localhost:3000',
     'webpack/hot/only-dev-server',
