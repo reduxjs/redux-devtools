d26dd0e29962fce295ac9b59b37f4a791a9e8e6b
Nathan Bierema <nbierema@gmail.com>
2020-08-04 23:54:02 -0400

    Finish

--- packages/d3-state-visualizer/examples/tree/webpack.config.js
+++ packages/d3-state-visualizer/examples/tree/webpack.config.js
@@ -3,7 +3,7 @@ var webpack = require('webpack');
 
 module.exports = {
   mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
-  devtool: 'eval',
+  devtool: 'eval-source-map',
   entry: [
     'webpack-dev-server/client?http://localhost:3000',
     'webpack/hot/only-dev-server',
