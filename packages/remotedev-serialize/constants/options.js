68826ebb09fa0470ffa519edafa0466c068f0054
Nathan Bierema <nbierema@gmail.com>
2020-08-06 11:39:08 -0400

    Add remotedev-serialize

---
+++ packages/remotedev-serialize/constants/options.js
@@ -0,0 +1,15 @@
+// jsan stringify options
+
+module.exports = {
+  refs: false, // references can't be resolved on the original Immutable structure
+  date: true,
+  function: true,
+  regex: true,
+  undefined: true,
+  error: true,
+  symbol: true,
+  map: true,
+  set: true,
+  nan: true,
+  infinity: true
+};
---
+++ packages/remotedev-serialize/test/helpers.spec.js
@@ -0,0 +1,27 @@
+var helpers = require('../helpers');
+var mark = helpers.mark;
+var extract = helpers.extract;
+var refer = helpers.refer;
+
+describe('Helpers', function() {
+  it('mark', function() {
+    expect(mark({ testData: 'test' }, 'testType')).toMatchSnapshot();
+    expect(
+      mark({ testData: 'test' }, 'testType', 'toString')
+    ).toMatchSnapshot();
+  });
+
+  it('extract', function() {
+    expect(extract({ testData: 'test' }, 'testType')).toMatchSnapshot();
+  });
+
+  it('refer', function() {
+    var TestClass = function(data) {
+      return data;
+    };
+    var testInstance = new TestClass({ testData: 'test' });
+    expect(
+      refer(testInstance, 'testType', false, [TestClass])
+    ).toMatchSnapshot();
+  });
+});
