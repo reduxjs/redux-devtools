6ad5b04e1486580529234a79a51d6280044cc310
Nathan Bierema <nbierema@gmail.com>
2020-08-08 11:10:40 -0400

    chore(*): unify react-base16-styling versions (#566)

--- packages/react-json-tree/src/index.js
+++ packages/react-json-tree/src/index.js
@@ -62,18 +62,9 @@ function checkLegacyTheming(theme, props) {
 function getStateFromProps(props) {
   let theme = checkLegacyTheming(props.theme, props);
   if (props.invertTheme) {
-    if (typeof theme === 'string') {
-      theme = `${theme}:inverted`;
-    } else if (theme && theme.extend) {
-      if (typeof theme === 'string') {
-        theme = { ...theme, extend: `${theme.extend}:inverted` };
-      } else {
-        theme = { ...theme, extend: invertTheme(theme.extend) };
-      }
-    } else if (theme) {
-      theme = invertTheme(theme);
-    }
+    theme = invertTheme(theme);
   }
+
   return {
     styling: createStylingFromTheme(theme)
   };
