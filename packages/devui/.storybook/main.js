fd6d9a8cfdec484e4410366c9fc61627636f3947
Nathan Bierema <nbierema@gmail.com>
2020-08-31 17:49:56 -0400

    stash

--- packages/devui/.storybook/main.js
+++ packages/devui/.storybook/main.js
@@ -1,7 +1,6 @@
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
-    '@storybook/addon-links',
     {
       name: '@storybook/addon-essentials',
       options: {
