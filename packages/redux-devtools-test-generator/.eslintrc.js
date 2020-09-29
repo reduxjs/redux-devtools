a44ad5d0e36372cef1baea93b1098f2fddd4f7f6
Nathan Bierema <nbierema@gmail.com>
2020-09-29 09:09:50 -0400

    Revert that file

--- packages/redux-devtools-test-generator/.eslintrc.js
+++ packages/redux-devtools-test-generator/.eslintrc.js
@@ -10,6 +10,22 @@ module.exports = {
       },
     },
     {
+      files: ['demo/**/*.ts', 'demo/**/*.tsx'],
+      extends: '../../eslintrc.ts.react.base.json',
+      parserOptions: {
+        tsconfigRootDir: __dirname,
+        project: ['./demo/tsconfig.json'],
+      },
+    },
+    {
+      files: ['demo/config/webpack.config.ts'],
+      extends: '../../eslintrc.ts.base.json',
+      parserOptions: {
+        tsconfigRootDir: __dirname,
+        project: ['./demo/config/tsconfig.json'],
+      },
+    },
+    {
       files: ['test/*.ts', 'test/*.tsx'],
       extends: '../../eslintrc.ts.react.jest.base.json',
       parserOptions: {
