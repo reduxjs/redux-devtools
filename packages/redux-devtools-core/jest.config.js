5dfea6e6bdabc9e32d4aae999f498f11b81ff1a8
Nathan Bierema <nbierema@gmail.com>
2020-10-25 19:20:30 -0400

    stash

--- packages/redux-devtools-core/jest.config.js
+++ packages/redux-devtools-core/jest.config.js
@@ -1,7 +1,4 @@
 module.exports = {
   preset: 'ts-jest',
   setupFilesAfterEnv: ['<rootDir>/test/setup.ts'],
-  moduleNameMapper: {
-    '\\.(css|scss)$': '<rootDir>/test/__mocks__/styleMock.ts',
-  },
 };
