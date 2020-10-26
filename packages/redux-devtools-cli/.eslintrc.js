ae403ce4882d8775516020e814470d32fe61b3b0
Nathan Bierema <nbierema@gmail.com>
2020-10-26 01:30:11 -0400

    Fix eslintrc

--- packages/redux-devtools-cli/.eslintrc.js
+++ packages/redux-devtools-cli/.eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
   overrides: [
     {
       files: ['*.ts', '*.tsx'],
-      extends: '../../eslintrc.ts.react.base.json',
+      extends: '../../eslintrc.ts.base.json',
       parserOptions: {
         tsconfigRootDir: __dirname,
         project: ['./tsconfig.json'],
@@ -11,18 +11,10 @@ module.exports = {
     },
     {
       files: ['test/*.ts', 'test/*.tsx'],
-      extends: '../../eslintrc.ts.react.jest.base.json',
+      extends: '../../eslintrc.ts.jest.base.json',
       parserOptions: {
         tsconfigRootDir: __dirname,
         project: ['./test/tsconfig.json'],
-      },
-    },
-    {
-      files: ['webpack.config.ts', 'webpack.config.umd.ts'],
-      extends: '../../eslintrc.ts.base.json',
-      parserOptions: {
-        tsconfigRootDir: __dirname,
-        project: ['./tsconfig.webpack.json'],
       },
     },
   ],
