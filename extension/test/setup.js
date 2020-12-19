8d6a529ea516e2248435330000b58ca1051c0d4c
Nathan Bierema <nbierema@gmail.com>
2020-12-19 15:19:51 -0500

    cross-env and enzyme

--- extension/test/setup.js
+++ extension/test/setup.js
@@ -1,2 +1,6 @@
 require('@babel/polyfill');
 global.chrome = require('sinon-chrome');
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-15.4';
+
+Enzyme.configure({ adapter: new Adapter() });
