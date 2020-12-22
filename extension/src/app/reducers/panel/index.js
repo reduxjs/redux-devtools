3ba4b003556d859099819b9550e8490c2c381216
Nathan Bierema <nbierema@gmail.com>
2020-12-22 17:02:10 -0500

    Fix some stuff

--- extension/src/app/reducers/panel/index.js
+++ extension/src/app/reducers/panel/index.js
@@ -2,13 +2,11 @@ import { combineReducers } from 'redux';
 import instances from '@redux-devtools/app/lib/reducers/instances';
 import monitor from '@redux-devtools/app/lib/reducers/monitor';
 import notification from '@redux-devtools/app/lib/reducers/notification';
-import test from '@redux-devtools/app/lib/reducers/test';
 import reports from '@redux-devtools/app/lib/reducers/reports';
 
 const rootReducer = combineReducers({
   instances,
   monitor,
-  test,
   reports,
   notification,
 });
--- extension/src/app/reducers/window/index.js
+++ extension/src/app/reducers/window/index.js
@@ -4,12 +4,10 @@ import monitor from '@redux-devtools/app/lib/reducers/monitor';
 import notification from '@redux-devtools/app/lib/reducers/notification';
 import socket from '@redux-devtools/app/lib/reducers/socket';
 import reports from '@redux-devtools/app/lib/reducers/reports';
-import test from '@redux-devtools/app/lib/reducers/test';
 
 const rootReducer = combineReducers({
   instances,
   monitor,
-  test,
   socket,
   reports,
   notification,
