786ee1173f97f1b21feb789d1948d0b3cc3659d6
Nathan Bierema <nbierema@gmail.com>
2021-06-06 15:03:09 -0400

    chore(cli): upgrade Electron

--- packages/redux-devtools-cli/app/electron.js
+++ packages/redux-devtools-cli/app/electron.js
@@ -3,48 +3,23 @@
 const { app, BrowserWindow } = require('electron');
 const argv = require('minimist')(process.argv.slice(2));
 
-let mainWindow;
-
 function createWindow() {
-  mainWindow = new BrowserWindow({
+  const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
-    webPreferences: {
-      nodeIntegration: false,
-      contextIsolation: true,
-      sandbox: true,
-      webSecurity: true,
-    },
   });
 
-  // mainWindow.loadFile('index.html')
   mainWindow.loadURL('http://localhost:' + (argv.port ? argv.port : 8000));
-
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
-
-  mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null;
-  });
 }
 
-app.on('ready', createWindow);
+app.whenReady().then(() => {
+  createWindow();
 
-app.on('window-all-closed', function () {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
 });
 
-app.on('activate', function () {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow();
-  }
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit();
 });
