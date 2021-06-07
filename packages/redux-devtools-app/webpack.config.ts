c1066b4273e151a4dc50da92571517952dff839e
Nathan Bierema <nbierema@gmail.com>
2021-06-07 00:13:02 -0400

    Revert port change

--- packages/redux-devtools-app/webpack.config.ts
+++ packages/redux-devtools-app/webpack.config.ts
@@ -82,7 +82,7 @@ module.exports = (env: { development?: boolean; platform?: string } = {}) => ({
     hints: false,
   },
   devServer: {
-    port: 3001,
+    port: 3000,
   },
   devtool: env.development ? 'eval-source-map' : 'source-map',
 });
