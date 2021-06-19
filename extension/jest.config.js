d1ab14c74e85263635e87bf6032f6ec29370e71c
Nathan Bierema <nbierema@gmail.com>
2021-06-19 17:36:16 -0400

    Test electron

--- extension/jest.config.js
+++ extension/jest.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   setupFilesAfterEnv: ['<rootDir>/test/setup.js'],
-  testPathIgnorePatterns: ['<rootDir>/examples', '<rootDir>/test/electron'],
+  testPathIgnorePatterns: ['<rootDir>/examples', '<rootDir>/test/chrome'],
 };
