6c3db1eae68d6fce46b1ddbadd5fa85f306d9086
FaberVitale <faber.vitale@gmail.com>
2021-08-26 20:06:03 +0200

    fix(rtk-query): demo build failing caused by a typing error

--- packages/redux-devtools-rtk-query-monitor/demo/src/store.ts
+++ packages/redux-devtools-rtk-query-monitor/demo/src/store.ts
@@ -1,4 +1,9 @@
-import { configureStore, Middleware } from '@reduxjs/toolkit';
+import {
+  configureStore,
+  Middleware,
+  combineReducers,
+  EnhancedStore,
+} from '@reduxjs/toolkit';
 import { pokemonApi } from './services/pokemon';
 import { postsApi } from 'services/posts';
 import DevTools from './features/DevTools/DevTools';
@@ -6,17 +11,19 @@ import { isExtensionEnabled } from 'features/DevTools/helpers';
 
 const devTools = isExtensionEnabled();
 
-export const store = configureStore({
-  reducer: {
-    [pokemonApi.reducerPath]: pokemonApi.reducer,
-    [postsApi.reducerPath]: postsApi.reducer,
-  },
+const reducer = combineReducers({
+  [pokemonApi.reducerPath]: pokemonApi.reducer,
+  [postsApi.reducerPath]: postsApi.reducer,
+});
+
+export const store: EnhancedStore<ReturnType<typeof reducer>> = configureStore({
+  reducer,
   devTools,
   // adding the api middleware enables caching, invalidation, polling and other features of `rtk-query`
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat([
       pokemonApi.middleware,
       postsApi.middleware,
-    ] as Middleware[]),
-  enhancers: devTools ? [] : [DevTools.instrument()],
+    ]) as Middleware[],
+  enhancers: (devTools ? [] : [DevTools.instrument()]) as any,
 });
