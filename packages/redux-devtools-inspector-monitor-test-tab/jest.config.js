fc647955371f75fb709f2e41f58c7fa0f525c3d6
Nathan Bierema <nbierema@gmail.com>
2021-08-29 21:08:39 -0400

    Fix importing css

--- packages/redux-devtools-inspector-monitor-test-tab/jest.config.js
+++ packages/redux-devtools-inspector-monitor-test-tab/jest.config.js
@@ -2,4 +2,7 @@ module.exports = {
   preset: 'ts-jest',
   setupFilesAfterEnv: ['<rootDir>/test/setup.ts'],
   testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '\\.css$': '<rootDir>/test/__mocks__/styleMock.ts',
+  },
 };
