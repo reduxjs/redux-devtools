39e20d5e30c77464b528c925a1e95b60bdcf686a
Nathan Bierema <nbierema@gmail.com>
2021-08-29 21:12:54 -0400

    More

--- extension/jest.config.js
+++ extension/jest.config.js
@@ -2,4 +2,7 @@ module.exports = {
   setupFilesAfterEnv: ['<rootDir>/test/setup.js'],
   testPathIgnorePatterns: ['<rootDir>/examples'],
   testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '\\.css$': '<rootDir>/test/__mocks__/styleMock.ts',
+  },
 };
