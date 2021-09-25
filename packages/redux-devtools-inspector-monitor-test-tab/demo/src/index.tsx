3d780414ae65de8ebc99b445298eeea3f9dcc07f
Nathan Bierema <nbierema@gmail.com>
2021-09-25 00:13:10 -0400

    stash

--- packages/redux-devtools-inspector-monitor-test-tab/demo/src/index.tsx
+++ packages/redux-devtools-inspector-monitor-test-tab/demo/src/index.tsx
@@ -1,4 +1,3 @@
-import '@redux-devtools/ui/lib/presets';
 import React from 'react';
 import { render } from 'react-dom';
 import { Container } from '@redux-devtools/ui';
--- packages/redux-devtools-inspector-monitor-test-tab/demo/webpack.config.ts
+++ packages/redux-devtools-inspector-monitor-test-tab/demo/webpack.config.ts
@@ -1,73 +1,50 @@
 import * as path from 'path';
-import * as webpack from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
-import { CleanWebpackPlugin } from 'clean-webpack-plugin';
 import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
-import pkg from '../../package.json';
-
-const isProduction = process.env.NODE_ENV === 'production';
+import pkg from '@redux-devtools/inspector-monitor-test-tab/package.json';
 
 module.exports = {
-  mode: process.env.NODE_ENV || 'development',
-  entry: isProduction
-    ? ['./demo/src/js/index']
-    : [
-        'webpack-dev-server/client?http://localhost:3000',
-        'webpack/hot/only-dev-server',
-        './demo/src/js/index',
-      ],
+  mode: 'development',
+  entry: './src/index.tsx',
+  devtool: 'eval-source-map',
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      package: pkg,
+    }),
+    new ForkTsCheckerWebpackPlugin(),
+  ],
   output: {
-    path: path.join(__dirname, 'demo/dist'),
-    filename: 'js/bundle.js',
+    filename: 'bundle.js',
+    path: path.join(__dirname, 'dist'),
+    clean: true,
   },
   module: {
     rules: [
       {
         test: /\.(js|ts)x?$/,
-        loader: 'babel-loader',
-        include: [
-          path.join(__dirname, '../../src'),
-          path.join(__dirname, '../src/js'),
-        ],
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: 'defaults' }],
+              '@babel/preset-react',
+              '@babel/preset-typescript',
+            ],
+          },
+        },
       },
       {
-        test: /\.css$/,
-        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
-      },
-      {
-        test: /\.(ttf|eot|svg|woff|woff2)$/,
-        loader: 'file-loader',
-        options: { outputPath: 'fonts/', publicPath: 'fonts/' },
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
       },
     ],
   },
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx'],
   },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      inject: true,
-      template: 'demo/src/index.html',
-      package: pkg,
-    }),
-    new ForkTsCheckerWebpackPlugin({
-      typescript: {
-        configFile: 'demo/tsconfig.json',
-      },
-    }),
-  ].concat(isProduction ? [] : [new webpack.HotModuleReplacementPlugin()]),
-  devServer: isProduction
-    ? null
-    : {
-        quiet: false,
-        port: 3001,
-        hot: true,
-        stats: {
-          chunkModules: false,
-          colors: true,
-        },
-        historyApiFallback: true,
-      },
-  devtool: 'eval-source-map',
 };
