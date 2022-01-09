4a8c9ef7c9b3b7b26930ebbdbf4ec668a6db0b18
Nathan Bierema <nbierema@gmail.com>
2022-01-08 22:53:19 -0500

    Use rollup for react-base16-styling

---
+++ packages/react-base16-styling/rollup.config.js
@@ -0,0 +1,24 @@
+import typescript from 'rollup-plugin-typescript2';
+import babel from '@rollup/plugin-babel';
+
+const config = [
+  {
+    input: 'src/index.ts',
+    output: [
+      { file: 'dist/react-base16-styling.cjs.js', format: 'cjs' },
+      { file: 'dist/react-base16-styling.esm.js', format: 'esm' },
+    ],
+    plugins: [
+      typescript(),
+      babel({
+        exclude: 'node_modules/**',
+        babelHelpers: 'runtime',
+        extensions: ['.ts'],
+        plugins: ['@babel/plugin-transform-runtime'],
+      }),
+    ],
+    external: [/@babel\/runtime/, 'base16', 'color', 'lodash.curry'],
+  },
+];
+
+export default config;
