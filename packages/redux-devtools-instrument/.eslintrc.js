698f336fd85434fb4a82bcd6d37dec09605812b2
Nathan Bierema <nbierema@gmail.com>
2022-01-10 01:03:18 -0500

    Fix eslint

--- packages/redux-devtools-instrument/.eslintrc.js
+++ packages/redux-devtools-instrument/.eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   extends: '../../eslintrc.js.base.json',
   overrides: [
     {
+      files: ['*.ts'],
       extends: '../../eslintrc.ts.base.json',
       parserOptions: {
         tsconfigRootDir: __dirname,
