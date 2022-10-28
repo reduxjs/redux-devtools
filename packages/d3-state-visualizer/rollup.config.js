5cf6886783932e549c5a785f05705e0f6dfbe9ec
Nathan Bierema <nbierema@gmail.com>
2022-10-28 10:03:57 -0400

    Switch to @rollup/plugin-terser (#1258)

--- packages/d3-state-visualizer/rollup.config.js
+++ packages/d3-state-visualizer/rollup.config.js
@@ -2,7 +2,7 @@ import typescript from 'rollup-plugin-typescript2';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import babel from '@rollup/plugin-babel';
-import { terser } from 'rollup-plugin-terser';
+import terser from '@rollup/plugin-terser';
 
 const config = [
   {
