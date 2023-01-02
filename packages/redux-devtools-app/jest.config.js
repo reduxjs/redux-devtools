0af27ca0683c9fd3f0618b33a366f9f6701799d4
Nathan Bierema <nbierema@gmail.com>
2023-01-02 16:34:05 -0500

    Fix app tests

--- packages/redux-devtools-app/jest.config.js
+++ packages/redux-devtools-app/jest.config.js
@@ -6,7 +6,9 @@ module.exports = {
     '\\.css$': '<rootDir>/test/__mocks__/styleMock.ts',
   },
   transform: {
+    '^.+\\.jsx?$': 'babel-jest',
     '^.+\\.tsx?$': ['ts-jest', { tsconfig: 'tsconfig.test.json' }],
   },
+  transformIgnorePatterns: ['<rootDir>/node_modules/(?!(d3)/)'],
   resolver: '<rootDir>/jestResolver.js',
 };
