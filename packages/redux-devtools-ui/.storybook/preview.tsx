d64ea502a5aa30f165b812d35eb1dbecd2aef6e7
Nathan Bierema <nbierema@gmail.com>
2023-04-05 18:04:07 -0400

    Config updates

--- packages/redux-devtools-ui/.storybook/preview.tsx
+++ packages/redux-devtools-ui/.storybook/preview.tsx
@@ -1,40 +1,8 @@
 import React from 'react';
+import type { Preview } from '@storybook/react';
+
 import { Container } from '../src';
 import { listSchemes, listThemes } from '../src/utils/theme';
-
-export const parameters = {
-  actions: { argTypesRegex: '^on[A-Z].*' },
-};
-
-export const globalTypes = {
-  theme: {
-    name: 'Theme',
-    description: 'Global theme for components',
-    defaultValue: 'default',
-    toolbar: {
-      items: listThemes(),
-      showName: true,
-    },
-  },
-  scheme: {
-    name: 'Color Scheme',
-    description: 'Global color scheme for components',
-    defaultValue: 'default',
-    toolbar: {
-      items: listSchemes(),
-      showName: true,
-    },
-  },
-  color: {
-    name: 'Color',
-    description: 'Global color for components',
-    defaultValue: 'light',
-    toolbar: {
-      items: ['auto', 'light', 'dark'],
-      showName: true,
-    },
-  },
-};
 
 const withThemeProvider = (Story, context) => (
   <Container
@@ -47,4 +15,41 @@ const withThemeProvider = (Story, context) => (
     <Story {...context} />
   </Container>
 );
-export const decorators = [withThemeProvider];
+
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: '^on[A-Z].*' },
+  },
+  globalTypes: {
+    theme: {
+      name: 'Theme',
+      description: 'Global theme for components',
+      defaultValue: 'default',
+      toolbar: {
+        items: listThemes(),
+        showName: true,
+      },
+    },
+    scheme: {
+      name: 'Color Scheme',
+      description: 'Global color scheme for components',
+      defaultValue: 'default',
+      toolbar: {
+        items: listSchemes(),
+        showName: true,
+      },
+    },
+    color: {
+      name: 'Color',
+      description: 'Global color for components',
+      defaultValue: 'light',
+      toolbar: {
+        items: ['auto', 'light', 'dark'],
+        showName: true,
+      },
+    },
+  },
+  decorators: [withThemeProvider],
+};
+
+export default preview;
