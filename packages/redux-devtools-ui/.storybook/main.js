e3ffa28de7d9f48ef6abd05e6efdac8cffd49e11
Nathan Bierema <nbierema@gmail.com>
2023-04-05 17:56:56 -0400

    Migrate

--- packages/redux-devtools-ui/.storybook/main.js
+++ packages/redux-devtools-ui/.storybook/main.js
@@ -1,14 +1,13 @@
 module.exports = {
-  core: {
-    builder: 'webpack5',
-  },
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: [
-    {
-      name: '@storybook/addon-essentials',
-      options: {
-        backgrounds: false,
-      },
-    },
-  ],
+  addons: [{
+    name: '@storybook/addon-essentials',
+    options: {
+      backgrounds: false
+    }
+  }],
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {}
+  }
 };
