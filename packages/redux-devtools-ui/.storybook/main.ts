bfcdc0771e0f653a88e6196388a25ee567d35c6d
Nathan Bierema <nbierema@gmail.com>
2024-04-01 16:37:26 -0400

    Disable background

--- packages/redux-devtools-ui/.storybook/main.ts
+++ packages/redux-devtools-ui/.storybook/main.ts
@@ -15,7 +15,12 @@ const config: StorybookConfig = {
     getAbsolutePath('@storybook/addon-webpack5-compiler-swc'),
     getAbsolutePath('@storybook/addon-onboarding'),
     getAbsolutePath('@storybook/addon-links'),
-    getAbsolutePath('@storybook/addon-essentials'),
+    {
+      name: getAbsolutePath('@storybook/addon-essentials'),
+      options: {
+        backgrounds: false,
+      },
+    },
     getAbsolutePath('@chromatic-com/storybook'),
     getAbsolutePath('@storybook/addon-interactions'),
   ],
