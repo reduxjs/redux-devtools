14b25d7e9b9385fb25b803261c85ead4cb8f83f5
Nathan Bierema <nbierema@gmail.com>
2024-08-05 22:01:17 -0400

    slider-monitor-example

--- packages/redux-devtools-slider-monitor/examples/todomvc/src/store/configureStore.ts
+++ packages/redux-devtools-slider-monitor/examples/todomvc/src/store/configureStore.ts
@@ -1,9 +1,9 @@
-import { PreloadedState, Store } from 'redux';
+import { Store } from 'redux';
 import { TodoState } from '../reducers';
 import { TodoAction } from '../actions/TodoActions';
 
 const configureStore: (
-  initialState?: PreloadedState<TodoState>,
+  initialState?: Partial<TodoState>,
 ) => Store<TodoState, TodoAction> =
   process.env.NODE_ENV === 'production'
     ? // eslint-disable-next-line @typescript-eslint/no-require-imports
