46e75e6c486f81ee953b507a76321a5d01740feb
Nathan Bierema <nbierema@gmail.com>
2024-08-07 23:18:46 -0400

    redux-devtools-app

---
+++ packages/redux-devtools-app/eslint.config.mjs
@@ -0,0 +1,33 @@
+import eslintJs from '../../eslint.js.config.base.mjs';
+import eslintTsReact from '../../eslint.ts.react.config.base.mjs';
+import eslintTsReactJest from '../../eslint.ts.react.jest.config.base.mjs';
+import eslintTs from '../../eslint.ts.react.config.base.mjs';
+
+export default [
+  ...eslintJs,
+  ...eslintTsReact(import.meta.dirname),
+  ...eslintTsReact(
+    import.meta.dirname,
+    ['demo/**/*.ts', 'demo/**/*.tsx'],
+    ['./tsconfig.demo.json'],
+  ),
+  ...eslintTsReactJest(['test/**/*.ts', 'test/**/*.tsx'], import.meta.dirname, [
+    './tsconfig.test.json',
+  ]),
+  ...eslintTs(
+    import.meta.dirname,
+    ['webpack.config.ts', 'webpack.config.umd.ts'],
+    ['./tsconfig.webpack.json'],
+  ),
+  {
+    ignores: ['build', 'lib', 'umd'],
+  },
+  {
+    files: ['buildUmd.mjs'],
+    languageOptions: {
+      globals: {
+        process: true,
+      },
+    },
+  },
+];
