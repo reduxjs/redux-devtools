446d6e8363b0a0d05da6e2a0c167f86e7fa406b9
Nathan Bierema <nbierema@gmail.com>
2025-06-01 09:41:50 -0400

    Fix

--- packages/redux-devtools-ui/src/Toolbar/styles/Divider.ts
+++ packages/redux-devtools-ui/src/Toolbar/styles/Divider.ts
@@ -1,8 +1,8 @@
 import styled from '@emotion/styled';
-import { Theme } from '../../themes/default';
+import { Base16Theme } from 'react-base16-styling';
 
 const Divider = styled.div`
-  background-color: ${(props: { theme?: Theme }) => props.theme!.base02};
+  background-color: ${(props: { theme?: Base16Theme & { inputHeight?: number } }) => props.theme!.base02};
   box-shadow: 1px 1px 2px ${(props) => props.theme.base00};
   height: ${(props) => props.theme.inputHeight || '30'}px;
   width: 1px;
