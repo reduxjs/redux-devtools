e202cef3b35fbf7b4c848896fa688bd491651c15
Nathan Bierema <nbierema@gmail.com>
2025-10-30 14:21:49 -0400

    ESM updates

--- packages/redux-devtools-ui/.storybook/main.ts
+++ packages/redux-devtools-ui/.storybook/main.ts
@@ -1,14 +1,18 @@
 import type { StorybookConfig } from '@storybook/react-webpack5';
 
-import { join, dirname } from 'path';
+import { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
 
 /**
  * This function is used to resolve the absolute path of a package.
  * It is needed in projects that use Yarn PnP or are set up within a monorepo.
  */
-function getAbsolutePath(value: string): any {
-  return dirname(require.resolve(join(value, 'package.json')));
+function getAbsolutePath(packageName: string) {
+  return dirname(
+    fileURLToPath(import.meta.resolve(`${packageName}/package.json`)),
+  );
 }
+
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [getAbsolutePath('@storybook/addon-onboarding')],
