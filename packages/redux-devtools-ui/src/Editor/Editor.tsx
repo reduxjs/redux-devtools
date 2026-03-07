321fbe19103d018e41f6dbed955ae122399c8db1
Nathan Bierema <nbierema@gmail.com>
2026-03-07 10:50:37 -0500

    Set theme styles

--- packages/redux-devtools-ui/src/Editor/Editor.tsx
+++ packages/redux-devtools-ui/src/Editor/Editor.tsx
@@ -44,7 +44,36 @@ export default function Editor({
             gutterActiveForeground: theme.base05,
             lineHighlight: 'transparent',
           },
-          styles: [],
+          styles: [
+            { tag: t.heading, color: theme.base05 },
+            { tag: t.quote, color: theme.base09 },
+            { tag: t.keyword, color: theme.base0F },
+            { tag: t.atom, color: theme.base0F },
+            { tag: t.number, color: theme.base0F },
+            { tag: t.definition(t.variableName), color: theme.base0D },
+            { tag: t.variableName, color: theme.base05 },
+            { tag: t.propertyName, color: theme.base0C },
+            { tag: t.operator, color: theme.base0E },
+            { tag: t.comment, color: theme.base05, fontStyle: 'italic' },
+            { tag: t.string, color: theme.base0B },
+            { tag: t.meta, color: theme.base0B },
+            { tag: t.bracket, color: theme.base09 },
+            { tag: t.tagName, color: theme.base02 },
+            { tag: t.attributeName, color: theme.base0C },
+            { tag: t.attributeName, color: theme.base02, cursor: 'pointer' },
+            {
+              tag: t.emphasis,
+              color: '#999',
+              textDecoration: 'underline',
+              textDecorationStyle: 'dotted',
+            },
+            { tag: t.strong, color: theme.base01 },
+            {
+              tag: t.invalid,
+              color: theme.base05,
+              borderBottom: `1px dotted ${theme.base08}`,
+            },
+          ],
         });
 
   return (
