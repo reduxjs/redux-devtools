8e8804672ad338186aacb87ff29b77c4a9066e72
Nathan Bierema <nbierema@gmail.com>
2026-03-07 15:47:44 -0500

    Updates

--- packages/redux-devtools-chart-monitor/src/index.ts
+++ packages/redux-devtools-chart-monitor/src/index.ts
@@ -1,1 +1,6 @@
-export { default as ChartMonitor } from './ChartMonitor.js';
+export {
+  default as ChartMonitor,
+  type ChartMonitorProps,
+} from './ChartMonitor.js';
+export { type ChartMonitorAction } from './actions.js';
+export { type ChartMonitorState } from './reducers.js';
