7f0994838472f8e28c83c1fb5a8c071312232d4e
Nathan Bierema <nbierema@gmail.com>
2026-03-07 14:56:37 -0500

    Fix redux-devtools test

--- packages/redux-devtools/jest.config.cjs
+++ packages/redux-devtools/jest.config.cjs
@@ -1,6 +1,10 @@
 module.exports = {
   preset: 'ts-jest',
   transform: {
+    '^.+\\.jsx?$': 'babel-jest',
     '^.+\\.tsx?$': ['ts-jest', { tsconfig: 'tsconfig.test.json' }],
   },
+  transformIgnorePatterns: [
+    'node_modules/(?!.pnpm|@redux-devtools/instrument)',
+  ],
 };
